14a1580442321cbb6019c9f187f4e407f32c583b
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2025-04-18 20:09:38 -0400

    added calc command

---
+++ commands/general/calc.js
@@ -0,0 +1,30 @@
+module.exports = [
+  // DONT GET RID OF INDEX!!!!!
+  // Done 4/18/25
+  {
+    name: "calc",
+    code: `
+      $clientTyping
+      $reply[$messageID;true]
+  
+  
+  
+      $color[1;#80bfff]
+      $title[1;
+          Here is your result:
+      ]
+      $description[1;
+          $math[$replaceText[$noMentionMessage;x;*]]
+      ]
+  
+  
+  
+      $onlyIf[$message!=;{"embeds":"
+          {newEmbed:
+          {title:Please include some expression to calculate!}
+          {color:#80bfff}}",
+          "reply": {"messageReference": "$messageID"}
+      }]
+      `,
+  },
+];
